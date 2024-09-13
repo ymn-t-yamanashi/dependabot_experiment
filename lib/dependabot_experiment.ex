ee68ea5560f29a7ff7eab3bc188ba34134956670
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-13 22:21:14 +0900

    mix new dependabot_experiment

---
+++ lib/dependabot_experiment.ex
@@ -0,0 +1,18 @@
+defmodule DependabotExperiment do
+  @moduledoc """
+  Documentation for `DependabotExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DependabotExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dependabot_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule DependabotExperimentTest do
+  use ExUnit.Case
+  doctest DependabotExperiment
+
+  test "greets the world" do
+    assert DependabotExperiment.hello() == :world
+  end
+end
